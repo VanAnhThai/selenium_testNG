10438ae9f84c1c9a16352ced40ba03d8ddeb45f3
thai.thi.van.anh <SUN-ASTERISK\thai.thi.van.anh@b123132-lt.sun-asterisk.com>
2023-05-31 19:13:12 +0900

    first commit

--- Test/testng/object/VibloPageObjects.java
+++ Test/testng/object/VibloPageObjects.java
@@ -2,6 +2,11 @@ package testng.object;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 
 import testng.object.BasicObject;
 
@@ -11,20 +16,49 @@ import testng.config.SetupDriver;
 
 public class VibloPageObjects extends BasicObject {
 
-	String URL = "https://accounts.viblo.asia/login";
+	private String accessUrl = "https://accounts.viblo.asia/login";
+
+	@FindBy(how = How.XPATH, using = "//input[@placeholder='Tên người dùng hoặc email']")
+	private WebElement emailInput;
+
+	@FindBy(how = How.XPATH, using = "//input[@type='password']")
+	private WebElement passwordInput;
+
+	@FindBy(how = How.XPATH, using = "/html/body/div/div/div/div/div/div/div[3]/button")
+	public WebElement loginButton;
+
+	@FindBy(how = How.XPATH, using = "/html/body/div[1]/div/div/div/div/div/div[3]/form/div[1]/div/div[2]")
+	public WebElement emailError;
+
+	@FindBy(how = How.XPATH, using = "/html/body/div[1]/div/div/div/div/div/div[3]/form/div[2]/div/div[2]")
+	public WebElement passwordError;
 
 	public VibloPageObjects() throws Exception {
 
 		this.driver = SetupDriver.buildSetupDriver();
-
-		driver.get(URL); // Initialise Elements
-		driver.manage().window().maximize(); // full screen
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		this.driver.get(accessUrl);
+		this.driver.manage().window().maximize();
+		PageFactory.initElements(driver, this);
 	}
 
-	public void sendValueForInputEmail(String email) {
-		WebElement inputEmail = driver
-				.findElement(By.xpath("/html/body/div/div/div/div/div/div/div[3]/form/div[1]/div/div/input"));
-		inputEmail.sendKeys(email);
+//	public void sendValueForInputEmail(String email) {
+//		WebElement inputEmail = driver
+//				.findElement(By.xpath("/html/body/div/div/div/div/div/div/div[3]/form/div[1]/div/div/input"));
+//		inputEmail.sendKeys(email);
+
+	public void clickLoginButton() {
+		loginButton.click();
+	}
+	public String emailInputError() {
+		return emailError.getText();
+	}
+
+	public String passwordInputError() {
+		return passwordError.getText();
+	}
+
+	public void quitDriver() {
+		// TODO Auto-generated method stub
+		
 	}
 }
---
+++ src/testng/test/VibloPageTestCasesTest.java
@@ -0,0 +1,133 @@
+package testng.test;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import testng.object.VibloPageObjects;
+
+public class VibloPageTestCasesTest {
+	VibloPageObjects vibloPage = null;
+
+	@Test
+	public void setupDriver() throws Exception {
+		vibloPage = new VibloPageObjects();
+
+	}
+
+// ----------- Test cases 1 ----------
+
+	@Test
+	public void tc1_CheckLoginPageTest() throws Exception {
+		String expectedTitle = "Viblo - Login";
+		String actualTitle = vibloPage.driver.getTitle();
+		AssertJUnit.assertEquals(actualTitle, expectedTitle);
+
+//	có thể dùng	Assert.assertEquals(vibloPage.getTitle(), "Viblo - Login");
+	}
+
+// ----------- Test cases 2 ----------
+
+	@Test
+	public void tc2_CheckRequireAllitemTest() throws Exception {
+		VibloPageObjects vibloPageOjects = new VibloPageObjects();
+		vibloPageOjects.loginButton.click();
+		Thread.sleep(500);
+		AssertJUnit.assertEquals(vibloPage.emailInputError(), "User/email is requried");
+		AssertJUnit.assertEquals(vibloPage.passwordInputError(), "Password is requried");
+	}
+
+// ----------- Test cases 3 ----------
+
+	@Test
+	public void tc3_CheckRequireUserTest() throws Exception {
+		WebElement dangNhapButton = vibloPage.findTxtObject("ByXpath",
+				"//button[@class='el-button w-100 el-button--primary']");
+		WebElement passWordTextBox = vibloPage.findTxtObject("ByXpath", "//input[@type='password']");
+		vibloPage.sendKeyForObject(passWordTextBox, "abc123456");
+
+		vibloPage.clickButton(dangNhapButton);
+
+		WebElement userError = vibloPage.findTxtObject("ByXpath",
+				"//*[contains(text(),'Tên người dùng/email là bắt buộc')]");
+		Assert.assertEquals(userError.getText(), "Tên người dùng/email là bắt buộc");
+		List<WebElement> listOfElements = vibloPage.driver
+				.findElements(By.xpath("//div[@class='el-form-item__error']"));
+		Assert.assertEquals(listOfElements.size(), 1);
+		Thread.sleep(2000);
+	}
+
+// ----------- Test cases 4 ----------
+
+	@Test
+	public void tc4_CheckRequirePassWordTest() {
+		WebElement dangNhapButton = vibloPage.findTxtObject("ByXpath",
+				"//button[@class='el-button w-100 el-button--primary']");
+		WebElement userTextBox = vibloPage.findTxtObject("ByXpath", "//input[@type='text']");
+		WebElement passWordTextBox = vibloPage.findTxtObject("ByXpath", "//input[@type='password']");
+
+		vibloPage.clearData(passWordTextBox);
+		vibloPage.sendKeyForObject(userTextBox, "anhttv");
+		vibloPage.clickButton(dangNhapButton);
+		WebElement passError = vibloPage.findTxtObject("ByXpath", "//*[contains(text(),'Mật khẩu là bắt buộc')]");
+
+		List<WebElement> listOfElements = vibloPage.driver
+				.findElements(By.xpath("//div[@class='el-form-item__error']"));
+		System.out.println("Number of elements:" + listOfElements.size());
+
+		Assert.assertEquals(listOfElements.size(), 1);
+		Assert.assertEquals(passError.getText(), "Mật khẩu là bắt buộc");
+	}
+
+// ----------- Test cases 5 ----------
+
+	@Test
+	public void tc5_CheckLoginSuccessTest() throws Exception {
+		WebElement dangNhapButton = vibloPage.findTxtObject("ByXpath",
+				"//button[@class='el-button w-100 el-button--primary']");
+		WebElement userTextBox = vibloPage.findTxtObject("ByXpath", "//input[@type='text']");
+		WebElement passWordTextBox = vibloPage.findTxtObject("ByXpath", "//input[@type='password']");
+
+		vibloPage.clearData(passWordTextBox);
+		vibloPage.clearData(userTextBox);
+
+		vibloPage.sendKeyForObject(userTextBox, "anhttv");
+		vibloPage.sendKeyForObject(passWordTextBox, "abc123456");
+
+		vibloPage.clickButton(dangNhapButton);
+
+		WebDriverWait wait = new WebDriverWait(vibloPage.driver, 20);
+		WebElement element = vibloPage.findTxtObject("ByXpath", "//*[contains(text(),'Chào mừng, Thai Thi Van Anh')]");
+
+		wait.until(ExpectedConditions.visibilityOf(element));
+
+		String expectedTitle = "Viblo Accounts";
+		String actualTitle = vibloPage.driver.getTitle();
+		Assert.assertEquals(actualTitle, expectedTitle);
+
+	}
+
+	@BeforeSuite
+	public void initializeDriver() throws Exception {
+		vibloPage = new VibloPageObjects();
+	}
+
+	@AfterSuite
+	public void quitDriver() {
+		if (vibloPage.driver != null) {
+			vibloPage.driver.quit();
+		}
+	}
+}
